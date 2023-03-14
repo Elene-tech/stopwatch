3d46d2d07f08d81ef5f984bff4bc8b6e89800d0f
Olena Kurilets <elenakurilets@gmail.com>
2023-03-14 12:09:06 +0100

    Add stopwatch function

--- src/Components/FormattedTime/FormattedTime.js
+++ src/Components/FormattedTime/FormattedTime.js
@@ -7,60 +7,47 @@ const FormattedTime = () => {
   const [min, setMin] = useState(0);
   const [second, setSecond] = useState(0);
   const [mSecond, setMsecond] = useState(0);
-  const [stop, setStop] = useState(true);
+  const [interval, setMsInterval] = useState(null);
 
   const onStart = () => {
-    setStop(false);
-    //setMsecond(mSecond + 1);
+    if (!interval) {
+      setMsInterval(
+        setInterval(() => {
+          setMsecond((state) => state + 100);
+        }, 100)
+      );
+    }
   };
 
   const onStop = () => {
-    setStop(true);
+    if (interval) {
+      clearInterval(interval);
+    }
   };
 
   const onReset = () => {
-    setHour(0);
-    setMin(0);
-    setSecond(0);
     setMsecond(0);
   };
 
-  useEffect(() => {
-    let interval = 0;
-    if (!stop) {
-      interval = setInterval(() => {
-        if (min > 59) {
-          setHour(hour + 1);
-          setMin(0);
-          clearInterval(interval);
-        }
-        if (second > 59) {
-          setMin(hour + 1);
-          setSecond(0);
-          clearInterval(interval);
-        }
-        if (mSecond > 999) {
-          setSecond(second + 1);
-          setMsecond(0);
-          clearInterval(interval);
-        }
-        if (mSecond <= 999) {
-          setMsecond(mSecond + 1);
-        }
-      }, 10);
-    } else {
-      clearInterval(interval);
-    }
-    return () => {
-      clearInterval(interval);
-    };
-  });
+  const getTimer = (mSecond) => {
+    let hours = mSecond / (1000 * 60 * 60);
+    let fullHour = Math.floor(hours);
+    let h = fullHour > 9 ? fullHour : '0' + fullHour;
+
+    let minutes = (hours - fullHour) * 60;
+    let fullMinute = Math.floor(minutes);
+    let m = fullMinute > 9 ? fullMinute : '0' + fullMinute;
+
+    let seconds = (minutes - fullMinute) * 60;
+    let fullSeconds = Math.floor(seconds);
+    let s = fullSeconds > 9 ? fullSeconds : '0' + fullSeconds;
+
+    return h + ':' + m + ':' + s;
+  };
 
   return (
     <div className={styles.formattedTime}>
-      <h1>
-        {hour} : {min} : {second} : {mSecond}
-      </h1>
+      <h1>{getTimer(mSecond)}</h1>
       <div className={styles.button_container}>
         <button className={styles.button} onClick={onStart}>
           Start
